6bc400af0b5cb2ee4fd2b34651361ff558c46e03
José Francisco Sesé <sesejose@gmail.com>
2025-02-10 14:12:26 +0100

    Defining URL params

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,53 @@
+import { json } from "@remix-run/node"; 
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
+// ---------- Create a Contact -----------
+// Importing a function to create a contact --> from data
+// Using ACTION from Remix to POST
+import { createEmptyContact } from "./data";
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return Response.json({ contact });
+};
+
+
+// ---------------- CSS and other links shoudl be like this -----------------
+// Importing the link function to later link the CSS 
+import type { LinksFunction } from "@remix-run/node";
+// importing the stylesheet / getting the url of it 
+import appStylesHref from "./app.css?url";
+// Exporting the links function that contains the url of the CSS previously imported 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+// ------------------ Importing Data -------------------
+// 1) Import json fom remix-run/node
+// 2) import useLoaderData together with others from remix-run/react
+// 3) Create and export a const variable for the loader = with an async arrow function as value to get the data
+// 4) Declare and Use the data with the useLoaderData in App
+// async arrow function to get the data and declare a variable for that
+import { getContacts } from "./data";
+import { remixVitePlugin } from "@remix-run/dev/dist/vite/plugin";
+// is Loader the function that fetch the data ?
+export const loader = async () => {
+  const contacts = await getContacts();
+  return Response.json({ contacts });
+};
+
 export default function App() {
+  // Here we use the declared variable with useLoaderData
+  // Note the Type Inference
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -34,15 +75,38 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+                {/* Link uses to= instead of href=  */}
+                {/* Client side routing allows our app to update the URL without requesting another document from the server. Instead, the app can immediately render new UI. */}
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+              
           </nav>
+        </div>
+        {/* In order for child routes to render inside of parent layouts, we need to import and render an Outlet in the parent.  */}
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
